307e7f4247e2227d67de64e3e2dbce8b559d3c6d
YERSIN <79632346+ersin08@users.noreply.github.com>
2022-07-12 17:49:40 +0600

    Update json_to_xml.py

--- json_to_xml.py
+++ json_to_xml.py
@@ -3,7 +3,7 @@ from json2xml.utils import readfromurl, readfromstring, readfromjson
 
 # get the xml from a json string
 
-# json-ды xml-ге конвертация жасайтын функция
+# Функция, которая конвертирует json в xml
 def j_2_xml(json_: str):
     data = readfromstring(json_)
     return json2xml.Json2xml(data).to_xml()
